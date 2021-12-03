669a9ae6a6bbf988eae0fa747fcd6cf195d5446f
Pedro Henrique de Souza Ramos <36521649+PedroSRamos@users.noreply.github.com>
2021-12-02 22:55:32 -0300

    commit

---
+++ SemProxyMethod/src/br/edu/univas/main/Main.java
@@ -0,0 +1,33 @@
+package br.edu.univas.main;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("...Criando Imagem...\n");
+		System.out.println("Abrindo arquivo Gatinho fofo do banco de dados.\n");
+		System.out.println("Desenhando: \n"
+				+ "	A___A \r\n"
+				+ " |・ㅅ・|  \r\n"
+				+ "	|っ　ｃ|  \r\n"
+				+ "	|　　 |  \r\n"
+				+ "	|　　　|  \r\n"
+				+ "	|　　　|  \r\n"
+				+ "	|　　　|  \r\n"
+				+ "	|　　　|  \r\n"
+				+ "	U￣￣U   \r\n"
+				+ "_Gatinho FOFO_");
+		
+		System.out.println("\n\nRecuperando Imagem Gatinho fofo criada anteriormente...\n");
+		
+		System.out.println("Desenhando: \n"
+				+ "	A___A \r\n"
+				+ " |・ㅅ・|  \r\n"
+				+ "	|っ　ｃ|  \r\n"
+				+ "	|　　 |  \r\n"
+				+ "	|　　　|  \r\n"
+				+ "	|　　　|  \r\n"
+				+ "	|　　　|  \r\n"
+				+ "	|　　　|  \r\n"
+				+ "	U￣￣U   \r\n"
+				+ "_Gatinho FOFO_");
+	}
+}
